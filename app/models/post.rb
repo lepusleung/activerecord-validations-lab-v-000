4e37316bb9386d1febf25f07f9f5e8bdd0cdd932
Man Leung <lepusleung@gmail.com>
2018-06-18 14:32:12 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+    message: "%{value} is not a valid category. Please choose 'Fiction' or 'Non-Fiction'." }
+  validate :clickbait_title?
+
+  Clickbait = [/Won't Believe/, /Secret/, /Top [0-9]*/, /Guess/]
+
+  def clickbait_title?
+    if !Clickbait.detect {|w| w.match title}
+      errors.add(:title, "Title must have clickbait words in it.")
+    end
+  end
 end
